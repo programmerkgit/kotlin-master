c24477a8741e715aac94d30bbb58bfd39ce419e7
admin <k.kubo.private.mobile@gmail.com>
2020-04-11 15:21:55 +0900

    classes

--- classes/11.methods-override.kt
+++ classes/11.methods-override.kt
@@ -20,3 +20,7 @@ fun main() {
     }
     println(Derived(1, 2, 3).add())
 }
+
+/*
+* It means that, by the time of the base class constructor execution, the properties declared or overridden in the derived class are not yet initialized. If any of those properties are used in the base class initialization logic (either directly or indirectly, through another overridden open member implementation), it may lead to incorrect behavior or a runtime failure. When designing a base class, you should therefore avoid using open members in the constructors, property initializers, and init blocks.
+* */
