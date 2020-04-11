591be7e93872062b80bfe103bd896b962287aaa2
admin <k.kubo.private.mobile@gmail.com>
2020-04-11 13:51:20 +0900

    add snipetts

---
+++ classes/07.implicit-primary-constructor-delegation.kt
@@ -0,0 +1,11 @@
+fun main( ){
+
+    class A {
+        val a = "primary initialization is called".also(::println)
+        constructor(a: Int) {
+            println("secondary constructor is called")
+        }
+    }
+
+    A(1)
+}
